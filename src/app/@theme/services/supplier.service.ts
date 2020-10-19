f60b8988c650b266f6f0c182f696e8534d91efca
bhaktiGloryAutoTech <bhakti.autotech@gmail.com>
2020-10-19 18:37:46 +0530

    Add Supplier

--- src/app/@theme/services/supplier.service.ts
+++ src/app/@theme/services/supplier.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/observable';
+import { CommonService } from './common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierService {
 
-  constructor() { }
+
+  constructor(private httpClient:HttpClient, private commonService:CommonService) { }
+  private handleError(err:HttpErrorResponse){
+    console.log("Handle Error Http call");
+    console.log(err.message);
+  }
+  addSupplierInSystem(supplierData){
+    return this.httpClient.post(this.commonService.envUrl()+'/api/supplier',supplierData);
+  }
 }
--- src/app/pages/supplier/add-edit-supplier/add-edit-supplier.component.ts
+++ src/app/pages/supplier/add-edit-supplier/add-edit-supplier.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { CommonService } from 'app/@theme/services/common.service';
+import { SupplierService } from 'app/@theme/services/supplier.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-add-edit-supplier',
@@ -10,26 +13,26 @@ import { Validators } from '@angular/forms';
   providers:[Location]
 })
 export class AddEditSupplierComponent implements OnInit {
-
-  public discountPercentage:number
-  public gstPercentage:number 
-  public paymentTerms:number 
-  public remark:string
-  public supplierName:string
   public createdBy:string 
+  user:any
   public userId:number 
   addSupplier:FormGroup;
   formSubmitted:boolean=false;
 
-  constructor(private location:Location) { }
+  constructor(private location:Location, private commonService:CommonService, private supplierService:SupplierService, private router:Router) { }
 
   ngOnInit(): void {
+    this.user = this.commonService.getUser();
+
     this.addSupplier = new FormGroup({
+      "supplierName": new FormControl(null,Validators.required),
       "discountPercentage": new FormControl(null,Validators.required),
       "gstPercentage": new FormControl(null,Validators.required),
       "paymentTerms": new FormControl(null,Validators.required),
-      "remark": new FormControl(null,),
-      "supplierName": new FormControl(null,Validators.required),
+      "remark": new FormControl(null),
+      "userId": new FormControl(this.user.userId),
+      "createdBy": new FormControl(this.user.userId)
+      
     }) 
   }
 
@@ -37,6 +40,22 @@ export class AddEditSupplierComponent implements OnInit {
     this.formSubmitted=true;
     if(this.addSupplier.valid){
       console.log("submitted");  
+      console.log(this.addSupplier.value)
+      console.log(this.addSupplier.get('userId'))
+      this.supplierService.addSupplierInSystem(this.addSupplier.value).subscribe(
+        data =>{
+          alert("Supplier Added Successfully")
+          console.log(data)
+          setTimeout(()=>{
+            this.router.navigate(['pages/supplier']);
+          },1000)
+        },
+        error=>{
+          console.log("Some Error Occure");
+          console.log(error.errorMessage);
+          alert("Some Error Occured");
+        }
+      )
     }
     else{
       return;  
