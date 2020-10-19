fd8910cd33767a5924d460b1de8b92a313da43a6
bhaktiGloryAutoTech <bhakti.autotech@gmail.com>
2020-10-19 18:56:56 +0530

    Add Supplier

--- src/app/@theme/services/supplier.service.ts
+++ src/app/@theme/services/supplier.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable} from 'rxjs/observable';
 import { CommonService } from './common.service';
 
 @Injectable({
