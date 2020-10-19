5ba54c2b533c0fa902968eee18d64330fa3e330a
Sagar <34083924+Sgrnk1z@users.noreply.github.com>
2020-10-19 18:21:15 +0530

    Add quality form operation

--- src/app/@theme/services/quality.service.ts
+++ src/app/@theme/services/quality.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { CommonService } from './common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QualityService {
-  constructor() { }
+  constructor(private httpClient:HttpClient,private commonService:CommonService) { }
+
+  addQuality(qualityData){
+    return this.httpClient.post(this.commonService.envUrl()+'/api/quality', qualityData);
+  }
 }
--- src/app/pages/quality/add-edit-quality/add-edit-quality.component.ts
+++ src/app/pages/quality/add-edit-quality/add-edit-quality.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonService } from 'app/@theme/services/common.service';
 import { PartyService } from 'app/@theme/services/party.service';
+import { QualityService } from 'app/@theme/services/quality.service';
 
 @Component({
   selector: 'ngx-add-edit-quality',
@@ -8,27 +11,24 @@ import { PartyService } from 'app/@theme/services/party.service';
   styleUrls: ['./add-edit-quality.component.scss']
 })
 export class AddEditQualityComponent implements OnInit {
-  private qualityID: number
-  private qualityName: string
-  private qualityType: String
-  private wtPer100m: number
-  private qualitySubType: string
-  private partyName: string
-  private remark: string
-  
+  formSubmitted: boolean = false
+  user: any
   addQualityForm: FormGroup
   party: any[];
-  constructor(private partyService: PartyService, private fbuilder: FormBuilder) { }
+  constructor(private partyService: PartyService, private commonService: CommonService, private qualityService: QualityService, private route:Router) { }
 
   ngOnInit(): void {
-    this.addQualityForm = this.fbuilder.group({
-      qualityId:[null, Validators.required],
-      qualityName:[null, Validators.required],
-      qualityType:['Select QualityType', Validators.required],
-      wtPer100m:[null],
-      qualitySubType:['Select Sub QualityType', Validators.required],
-      partyName:['Select Party Name',Validators.required],
-      remark:[null]
+    this.user = this.commonService.getUser()
+    this.addQualityForm = new FormGroup({
+      'qualityId':new FormControl(null, Validators.required),
+      'qualityName':new FormControl(null, Validators.required),
+      'qualityType':new FormControl(null, Validators.required),
+      'wtPer100m':new FormControl(null),
+      'qualitySubType':new FormControl(null, Validators.required),
+      'partyId':new FormControl(null,Validators.required),
+      'remark':new FormControl(null),
+      'createdBy':new FormControl(this.user.userId.toString()),
+      //'createdDate': new FormControl(new Date())                                    
     })
     this.getPartyList();
   }
@@ -49,8 +49,27 @@ export class AddEditQualityComponent implements OnInit {
   }
 
   onSubmit(){
-      if(this.addQualityForm.valid)
+    this.formSubmitted = true
+      if(this.addQualityForm.valid){
+        //this.addQualityForm.get('partyId').setValue()
+        console.log(this.addQualityForm.get('partyId').value)
+        this.qualityService.addQuality(this.addQualityForm.value).subscribe(
+          data =>{
+            console.log('Quality Added!');
+            alert('Quality added successfully!');
+            //console.log(data);
+            setTimeout(()=>{
+              this.route.navigate(['/pages/quality'])
+            },1000)
+          },
+          error=>{
+            console.log('Error occured');
+            console.log(error.errorMessage);           
+          }
+        )
+
         console.log("Success")
+      }
       else
         return ;
   }
