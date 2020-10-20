954cd41caee257874d9a00b5923b709512b6eaf5
Sagar <34083924+Sgrnk1z@users.noreply.github.com>
2020-10-20 18:37:13 +0530

    Quality Table

--- src/app/pages/quality/quality.component.ts
+++ src/app/pages/quality/quality.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QualityService } from '../../@theme/services/quality.service';
 
 @Component({
   selector: 'ngx-quality',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quality.component.scss']
 })
 export class QualityComponent implements OnInit {
-
-  constructor() { }
+  qualityList
+  tableStyle = 'bootstrap'
+  constructor(private qualityService: QualityService) { }
 
   ngOnInit(): void {
+    
+    this.qualityService.getallQuality().subscribe(
+      data =>{
+        this.qualityList = data['data']
+      },
+      error=>{
+        console.log(error)
+      }
+    )
   }
-
 }
