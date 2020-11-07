9e0e3b811e23382d9d89e244e230ec6265c4310f
shailajamadhvani <shailaja.autotech@gmail.com>
2020-11-07 16:12:07 +0530

    user page

---
+++ src/app/@theme/model/user.ts
@@ -0,0 +1,11 @@
+export class User{
+ firstName:String;
+ lastName:String;
+ userName:String;
+ email:String;
+ mobile:String;
+ password:String;
+ company:String;
+ department:String;
+ designation:String;
+}
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {NgbModal}  from '@ng-bootstrap/ng-bootstrap'; 
+import { ConfirmationDialogComponent } from 'app/@theme/components/confirmation-dialog/confirmation-dialog.component';
+import * as errorData from 'app/@theme/json/error.json';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'ngx-user',
@@ -6,8 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  public errorData: any = (errorData as any).default;
 
-  constructor() { }
+  tableStyle = 'bootstrap';
+  
+  userList=[];
+
+  constructor(
+    private route:Router,
+    private modalService: NgbModal,
+    private toastr:ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
