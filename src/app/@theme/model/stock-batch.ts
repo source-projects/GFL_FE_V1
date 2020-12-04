164666de717ac9d069f2788ba751067f330f6281
Bhakti Madia <bhaktimadia@github.com>
2020-12-04 18:44:27 +0530

    changes

--- src/app/@theme/model/stock-batch.ts
+++ src/app/@theme/model/stock-batch.ts
@@ -1,6 +1,5 @@
 export class StockBatch {
     id: number;
-<<<<<<< HEAD
     stockInType: string = "Fabric";
     partyId: number;
     qualityId: number;
@@ -15,22 +14,6 @@ export class StockBatch {
     updatedBy: Number;
     userHeadId: Number;
     batchData: BatchData[];
-=======
-    stockInType:string= "Fabric";
-    partyId:number;
-    qualityId:number;  
-    billNo:string;
-    billDate:Date;
-    chlNo:string;
-    unit:string;
-    chlDate:Date;
-    remark:string;
-    createdBy:Number;
-    createdDate: Date;
-    updatedBy:Number;
-    userHeadId:Number;
-    batchData:BatchData[];
->>>>>>> 21f01d9ef45838d2cd92ee3a56fc5ef03288d2af
     isProductionPlanned: boolean;
 }
 export class BatchData {
