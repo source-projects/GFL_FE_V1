fa845808ea4c6ced590a80bee078bcad2e514f7e
Bhakti Madia <bhaktimadia@github.com>
2020-12-12 12:37:54 +0530

    device-detect

--- src/app/pages/shade/shade.component.ts
+++ src/app/pages/shade/shade.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShadeService } from 'app/@theme/services/shade.service';
-import {NgbModal}  from '@ng-bootstrap/ng-bootstrap'; 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationDialogComponent } from 'app/@theme/components/confirmation-dialog/confirmation-dialog.component';
 import * as errorData from 'app/@theme/json/error.json';
 
@@ -22,35 +22,35 @@ export class ShadeComponent implements OnInit {
   public errorData: any = (errorData as any).default;
 
   tableStyle = 'bootstrap';
-  shadeList=[];
-  shade=[];
-  headers=["Party Shade No", "Process Name", "Quality Id", "Quality Name", "Party Name", "Color Tone" ];
+  shadeList = [];
+  shade = [];
+  headers = ["Party Shade No", "Process Name", "Quality Id", "Quality Name", "Party Name", "Color Tone"];
   radioSelect = 1;
   flag = false;
 
   radioArray = [
-    {id:1, value:"View Own"  , disabled:false},
-    {id:2, value:"View Group"  , disabled:false},
-    {id:3, value:"View All"  , disabled:false}
+    { id: 1, value: "View Own", disabled: false },
+    { id: 2, value: "View Group", disabled: false },
+    { id: 3, value: "View All", disabled: false }
   ];
   userHeadId;
   userId;
   permissions: Number;
 
 
-  hidden :boolean=true;
-  hiddenEdit:boolean=true;
-  hiddenView:boolean=true;
+  hidden: boolean = true;
+  hiddenEdit: boolean = true;
+  hiddenView: boolean = true;
 
-  ownDelete=true;
-  allDelete=true;
-  groupDelete=true;
+  ownDelete = true;
+  allDelete = true;
+  groupDelete = true;
 
-  ownEdit=true;
-  allEdit=true;
-  groupEdit=true;
- 
- 
+  ownEdit = true;
+  allEdit = true;
+  groupEdit = true;
+
+
   constructor(
     private shadeService: ShadeService,
     private route: Router,
@@ -61,9 +61,9 @@ export class ShadeComponent implements OnInit {
     private commonService: CommonService,
     private exportService: ExportService
   ) { }
- 
-  
-  
+
+
+
 
   ngOnInit(): void {
     this.userId = this.commonService.getUser();
@@ -71,51 +71,54 @@ export class ShadeComponent implements OnInit {
     this.userHeadId = this.commonService.getUserHeadId();
     this.userHeadId = this.userHeadId['userHeadId'];
     this.getViewAccess();
-    this.getallShades(this.userId,"own");
+    this.getallShades(this.userId, "own");
     this.getDeleteAccess();
     this.getEditAccess();
   }
 
-  onChange(event){
-    this.shadeList=[];
-    switch(event){
-      case 1: 
-              this.getallShades(this.userId,"own");
-              this.hidden=this.ownDelete; 
-              this.hiddenEdit=this.ownEdit;
-              break;
+  onChange(event) {
+    this.shadeList = [];
+    switch (event) {
+      case 1:
+        this.getallShades(this.userId, "own");
+        this.hidden = this.ownDelete;
+        this.hiddenEdit = this.ownEdit;
+        break;
 
-      case 2: 
-              this.getallShades(this.userHeadId,"group");
-              this.hidden=this.groupDelete;
-              this.hiddenEdit=this.groupEdit;
-              break;
+      case 2:
+        this.getallShades(this.userHeadId, "group");
+        this.hidden = this.groupDelete;
+        this.hiddenEdit = this.groupEdit;
+        break;
 
       case 3:
-              this.getallShades(0,"all");
-              this.hidden=this.allDelete;
-              this.hiddenEdit=this.allEdit;
-              break;
+        this.getallShades(0, "all");
+        this.hidden = this.allDelete;
+        this.hiddenEdit = this.allEdit;
+        break;
     }
   }
 
-  open(){
-    this.flag=true;
-   
+  open() {
+    this.flag = true;
+
     const modalRef = this.modalService.open(ExportPopupComponent);
-     modalRef.componentInstance.headers = this.headers;
-     modalRef.componentInstance.list = this.shade;
+    modalRef.componentInstance.headers = this.headers;
+    modalRef.componentInstance.list = this.shade;
   }
 
-  getallShades(id,getBy){
-  this.shadeService.getShadeMastList(id,getBy).subscribe(
-      data =>{
-        this.shadeList = data['data'];
-        this.shade=this.shadeList.map((element)=>({partyShadeNo:element.partyShadeNo, processName: element.processName,
-          qualityId: element.qualityId, qualityName:element.qualityName, partyName:element.partyName, colorTone:element.colorTone }))
-          console.log(this.shade);
+  getallShades(id, getBy) {
+    this.shadeService.getShadeMastList(id, getBy).subscribe(
+      data => {
+        if (data['data'] != null) {
+          this.shadeList = data['data'];
+          this.shade = this.shadeList.map((element) => ({
+            partyShadeNo: element.partyShadeNo, processName: element.processName,
+            qualityId: element.qualityId, qualityName: element.qualityName, partyName: element.partyName, colorTone: element.colorTone
+          }))
+        }
       },
-      error=>{
+      error => {
         this.toastr.error(errorData.Serever_Error)
       }
     );
@@ -141,52 +144,52 @@ export class ShadeComponent implements OnInit {
   }
 
 
-  getViewAccess(){
-    if(!this.shadeGuard.accessRights('view')){
-      this.radioArray[0].disabled=true;
+  getViewAccess() {
+    if (!this.shadeGuard.accessRights('view')) {
+      this.radioArray[0].disabled = true;
     }
     else
-    this.radioArray[0].disabled=false;
-     if(!this.shadeGuard.accessRights('view group')){
-      this.radioArray[1].disabled=true;
+      this.radioArray[0].disabled = false;
+    if (!this.shadeGuard.accessRights('view group')) {
+      this.radioArray[1].disabled = true;
     }
     else
-    this.radioArray[1].disabled=false;
-     if(!this.shadeGuard.accessRights('view all')){
-      this.radioArray[2].disabled=true;
+      this.radioArray[1].disabled = false;
+    if (!this.shadeGuard.accessRights('view all')) {
+      this.radioArray[2].disabled = true;
     }
     else
-    this.radioArray[2].disabled=false;
+      this.radioArray[2].disabled = false;
 
   }
 
-  getDeleteAccess(){
-    if(this.shadeGuard.accessRights('delete')){
-      this.ownDelete=false;
+  getDeleteAccess() {
+    if (this.shadeGuard.accessRights('delete')) {
+      this.ownDelete = false;
     }
-     if(this.shadeGuard.accessRights('delete group')){
-      this.groupDelete=false;
+    if (this.shadeGuard.accessRights('delete group')) {
+      this.groupDelete = false;
     }
-     if(this.shadeGuard.accessRights('delete all')){
-      this.allDelete=false;
+    if (this.shadeGuard.accessRights('delete all')) {
+      this.allDelete = false;
     }
   }
 
-  getEditAccess(){
-    if(this.shadeGuard.accessRights('edit')){
-      this.ownEdit=false;
+  getEditAccess() {
+    if (this.shadeGuard.accessRights('edit')) {
+      this.ownEdit = false;
     }
-     if(this.shadeGuard.accessRights('edit group')){
-      this.groupEdit=false;
+    if (this.shadeGuard.accessRights('edit group')) {
+      this.groupEdit = false;
 
     }
-     if(this.shadeGuard.accessRights('edit all')){
-      this.allEdit=false;
+    if (this.shadeGuard.accessRights('edit all')) {
+      this.allEdit = false;
     }
   }
 
 
 }
- 
+
 
 
