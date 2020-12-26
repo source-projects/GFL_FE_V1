a2dddc6cde94a7c841a508817ab48839a7958844
kushal2868 <kushal.autotech@gmail.com>
2020-12-26 13:34:28 +0530

    Created input data in menu for log-sheet

---
+++ src/app/pages/input-data/input-data.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputDataComponent } from './input-data/input-data.component';
+import {
+  NbButtonModule,
+  NbCardModule,
+
+
+
+  NbIconModule,
+
+  NbListModule, NbProgressBarModule,
+
+
+
+  NbSelectModule, NbTabsetModule,
+  NbUserModule
+} from '@nebular/theme';
+import { ThemeModule } from 'app/@theme/theme.module';
+import { SharedModule } from 'app/@theme/shared.module';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+
+
+@NgModule({
+  declarations: [InputDataComponent],
+  imports: [
+    CommonModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbIconModule,
+    NbTabsetModule,
+    NbSelectModule,
+    NbListModule,
+    NbProgressBarModule,
+    NgSelectModule,
+    SharedModule,
+    OwlDateTimeModule,
+    FormsModule,
+    OwlNativeDateTimeModule
+  ]
+})
+export class InputDataModule { }
