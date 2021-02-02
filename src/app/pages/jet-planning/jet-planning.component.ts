984ac2e59e11eddbad8ac2f6705e2ba4a0f0652e
shailajamadhvani <shailaja.autotech@gmail.com>
2021-02-02 18:07:11 +0530

    jet planning changes

--- src/app/pages/jet-planning/jet-planning.component.ts
+++ src/app/pages/jet-planning/jet-planning.component.ts
@@ -78,9 +78,18 @@ export class JetPlanningComponent implements OnInit {
     private programService: ProgramService,
     private shadeService: ShadeService
 
-  ) { }
+  ) {
+
+    
+   }
 
   ngOnInit(): void {
+    this.productionPlanningService.addToJetClicked.subscribe(ele => {
+      console.log(ele);
+      if(ele){
+        this.batchSelected(ele);
+      }
+    })
     this.getCurrentId();
     this.getPartyList();
     this.getQualityList();
@@ -91,6 +100,7 @@ export class JetPlanningComponent implements OnInit {
     //this.getAllBatchWithShade();
   
   }
+
 
   
   getCurrentId() {
@@ -311,7 +321,12 @@ export class JetPlanningComponent implements OnInit {
     );}
 
     batchSelected(event){
-      let p_id= Number(event.target.value);
+      let p_id;
+      if(event.target){
+        p_id= Number(event.target.value);
+      }else{
+        p_id = event;
+      }
       const modalRef = this.modalService.open(ShadeWithBatchComponent).result.then(
         (result) => {
           
@@ -336,30 +351,27 @@ export class JetPlanningComponent implements OnInit {
            let arr =[];
           //  jetData2.productionId = Number(jetData2.productionId);
            arr.push(jetData2)
-          this.jetService.saveJetData(arr).subscribe(
-            (data) => {
-              if (data["success"]) {
-                this.toastr.success(errorData.Add_Success);
-                this.getJetData();
-                //this.getshade();
-              }
-              else {
-                this.toastr.error("Weight is more than jet capacity");
-                this.getJetData();
-                //this.getshade();
-              }
-
-            }
-          )
-
-          // if (this.jet[index].jetDataList == null) {
-          //   this.jet[index].jetDataList = this.jetData1;
-          // }
-          // else {
-          //   this.jet[index].jetDataList.push(this.jetData1);
-          // }
-
+           this.addJetData(arr);
+          
         })
+    }
+
+    addJetData(arr){
+      this.jetService.saveJetData(arr).subscribe(
+        (data) => {
+          if (data["success"]) {
+            this.toastr.success(errorData.Add_Success);
+            this.getJetData();
+            //this.getshade();
+          }
+          else {
+            this.toastr.error("Weight is more than jet capacity");
+            this.getJetData();
+            //this.getshade();
+          }
+
+        }
+      )
     }
 
   drop(event: CdkDragDrop<any[]>, i) {
@@ -421,51 +433,51 @@ export class JetPlanningComponent implements OnInit {
   
     }
   }
-  getshade() {
-    this.productionPlanningService.getAllProductionPlan().subscribe(
-      (data) => {
-        if (data["success"]) {
-          this.allShade = data["data"];
-        }
-        else {
-          // this.toastr.error(data["msg"]);
-          this.loading = false;
-          this.allShade = null;
-        }
-      },
-      (error) => {
-        // this.toastr.error(errorData.Serever_Error);
-        this.loading = false;
-        this.allShade = null;
-      }
-    );
+  // getshade() {
+  //   this.productionPlanningService.getAllProductionPlan().subscribe(
+  //     (data) => {
+  //       if (data["success"]) {
+  //         this.allShade = data["data"];
+  //       }
+  //       else {
+  //         // this.toastr.error(data["msg"]);
+  //         this.loading = false;
+  //         this.allShade = null;
+  //       }
+  //     },
+  //     (error) => {
+  //       // this.toastr.error(errorData.Serever_Error);
+  //       this.loading = false;
+  //       this.allShade = null;
+  //     }
+  //   );
 
-  }
+  // }
 
-  addNew(event, index) {
+  // addNew(event, index) {
 
-    if (this.allShade == null || this.allShade == undefined) {
-      const modalRef = this.modalService.open(WarningPopupComponent)
-    }
-    else {
-      const modalRef = this.modalService.open(ShadeWithBatchComponent).result.then(
-        (result) => {
-          //this.jet[index].production.push(result.batchId);
-          //this.jet[index]
-          // if(this.jet[index].jetDataList == null){
-          //   this.jet[index].jetDataList = result;
-          // }
-          // else{
-          //   this.jet[index].jetDataList.push(result);
+  //   if (this.allShade == null || this.allShade == undefined) {
+  //     const modalRef = this.modalService.open(WarningPopupComponent)
+  //   }
+  //   else {
+  //     const modalRef = this.modalService.open(ShadeWithBatchComponent).result.then(
+  //       (result) => {
+  //         //this.jet[index].production.push(result.batchId);
+  //         //this.jet[index]
+  //         // if(this.jet[index].jetDataList == null){
+  //         //   this.jet[index].jetDataList = result;
+  //         // }
+  //         // else{
+  //         //   this.jet[index].jetDataList.push(result);
 
-          // }
+  //         // }
           
-        });
+  //       });
 
-    }
+  //   }
 
 
-  }
+  // }
 
   getJetData() {
     this.loading = true;
