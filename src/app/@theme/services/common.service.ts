f942a6b4a69a0162dcfca53891e49712a3ccb63f
SagarGloryAutotech <72645682+SagarGloryAutotech@users.noreply.github.com>
2021-02-04 15:11:23 +0530

    Update common.service.ts

--- src/app/@theme/services/common.service.ts
+++ src/app/@theme/services/common.service.ts
@@ -38,7 +38,8 @@ export class CommonService {
     let location = window.location;
     if(location["hostname"] == "192.168.1.104"){
       url = "http://192.168.1.104:8080/";
-    }
+    }else if(location['hostname'] == "13.235.71.124" || location['hostname'] == "localhost")
+      url = "http://13.235.71.124:8080/";
     else url = "http://103.137.194.167:8080/";
     return url;
     //return url "http://13.235.71.124:8080/";
