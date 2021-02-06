b7af3db573eab2c33f5d39121bebc8432b125262
SagarGloryAutotech <sagar.gloryautotech@gmail.com>
2021-02-06 09:22:02 +0530

    invoice report UI

--- src/app/@theme/model/invoice.ts
+++ src/app/@theme/model/invoice.ts
@@ -22,10 +22,16 @@ export class Invoice {
 export class InvoiceReportRequest{
   userHeadId: number;
   partyId: number;
-  from: Date;
-  to: Date;
-}
+  from: any;
+  to: any;
 
+  constructor(){
+    this.userHeadId = null;
+    this.partyId = null;
+    this.from = "";
+    this.to = "";
+  }
+}
 export class DispatchData {
     batchEntryId:Number;
     controlId:Number;
@@ -42,4 +48,38 @@ export class invoiceobj{
  
   batchId:Number;
   stockId:Number;
+}
+
+
+//Detailed report invoice
+export class InvoiceDetailedReport {
+  headName: string;
+  invoiceNo: string;
+  partyId: number;
+  partyName: string;
+  qualityList: QualityList[];
+  userHeadId: number;
+}
+
+export class QualityList {
+  amt: number;
+  batchId: string;
+  qualityEntryId: number;
+  qulityId: string;
+  rate: number;
+  totalFinishMtr: number;
+  totalMtr: number;
+}
+
+
+//short invoice report
+export class InvoiceShortReport{
+  invoiceNo: string;
+  partyName: string;
+  partyId: number;
+  headName: string;
+  userHeadId: number;
+  amt: number;
+  totalFinishMtr: number;
+  totalBatchMtr: number;
 }
--- src/app/pages/generate-invoice/invoice-report/invoice-report.component.ts
+++ src/app/pages/generate-invoice/invoice-report/invoice-report.component.ts
@@ -1,50 +1,131 @@
-import { Component, OnInit } from '@angular/core';
-import { InvoiceReportRequest } from 'app/@theme/model/invoice';
-import { GenerateInvoiceService } from 'app/@theme/services/generate-invoice.service';
-import { PartyService } from 'app/@theme/services/party.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  InvoiceDetailedReport,
+  InvoiceReportRequest,
+  InvoiceShortReport,
+} from "app/@theme/model/invoice";
+import { GenerateInvoiceService } from "app/@theme/services/generate-invoice.service";
+import { PartyService } from "app/@theme/services/party.service";
+import * as wijmo from "@grapecity/wijmo";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'ngx-invoice-report',
-  templateUrl: './invoice-report.component.html',
-  styleUrls: ['./invoice-report.component.scss']
+  selector: "ngx-invoice-report",
+  templateUrl: "./invoice-report.component.html",
+  styleUrls: ["./invoice-report.component.scss"],
 })
 export class InvoiceReportComponent implements OnInit {
-
   public invoiceReportRequest: InvoiceReportRequest;
+  public maxDate: any;
+  public currentDate = new Date();
+  public disableButton: boolean = false;
+  public shortReport: InvoiceShortReport[] = [];
+  public detailedReport: InvoiceDetailedReport[] = [];
   public masterList = [];
   public partyList = [];
 
-  constructor(private invoiceService: GenerateInvoiceService, private partyService: PartyService) { 
+  constructor(
+    private invoiceService: GenerateInvoiceService,
+    private partyService: PartyService,
+    private router: Router
+  ) {
     this.invoiceReportRequest = new InvoiceReportRequest();
   }
 
   ngOnInit(): void {
+    this.maxDate = new Date(
+      this.currentDate.getFullYear(),
+      this.currentDate.getMonth(),
+      this.currentDate.getDate(),
+      23,
+      59
+    );
     this.getAllMasters();
     this.getAllParties();
   }
 
-  getAllMasters(){
+  getAllMasters() {
     this.partyService.getAllMaster().subscribe(
-      data=>{
-        if(data['success']){
-          this.masterList = data['data'];
+      (data) => {
+        if (data["success"]) {
+          this.masterList = data["data"];
         }
-      }, error=>{
-
-      }
-    )
+      },
+      (error) => {}
+    );
   }
 
-  getAllParties(){
-    this.partyService.getAllPartyList(0, 'all').subscribe(
-      data=>{
-        if(data['success']){
-          this.partyList = data['data'];
+  getAllParties() {
+    this.partyService.getAllPartyList(0, "all").subscribe(
+      (data) => {
+        if (data["success"]) {
+          this.partyList = data["data"];
         }
-      }, error=>{
-
-      }
-    )
+      },
+      (error) => {}
+    );
   }
 
+  getShortReport() {
+    this.shortReport = [];
+    this.detailedReport = [];
+    this.invoiceService
+      .getShortInvoiceReport(this.invoiceReportRequest)
+      .subscribe(
+        (data) => {
+          if (data["success"]) {
+            this.shortReport = data["data"];
+            this.printReport("short");
+          }
+        },
+        (error) => {}
+      );
+  }
+
+  getDetailedReport() {
+    this.shortReport = [];
+    this.detailedReport = [];
+    this.invoiceService
+      .getDetailedInvoiceReport(this.invoiceReportRequest)
+      .subscribe(
+        (data) => {
+          if (data["success"]) {
+            this.detailedReport = data["data"];
+            this.printReport("detailed");
+          }
+        },
+        (error) => {}
+      );
+  }
+
+  printReport(type) {
+    let doc = new wijmo.PrintDocument({
+      title: "",
+    });
+    doc.append(
+      '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.3.0/paper.css">'
+    );
+    doc.append(
+      '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">'
+    );
+    doc.append(
+      '<link href="https://cdn.grapecity.com/wijmo/5.latest/styles/wijmo.min.css" rel="stylesheet">'
+    );
+    let inter1 = setInterval(() => {
+      let data1;
+      if (type == "detailed") {
+        data1 = <HTMLElement>document.getElementById("detailedReport");
+      } else {
+        data1 = <HTMLElement>document.getElementById("shortReport");
+      }
+      if (data1 != null) {
+        doc.append(data1);
+        doc.print();
+        clearInterval(inter1);
+        this.shortReport = [];
+        this.detailedReport = [];
+        this.invoiceReportRequest = new InvoiceReportRequest();
+      }
+    }, 10);
+  }
 }
