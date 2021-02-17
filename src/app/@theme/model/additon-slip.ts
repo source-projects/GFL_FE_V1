2dfc5afb496d7b8ce13d357f7fe998213c8f9f65
shailajamadhvani <shailaja.autotech@gmail.com>
2021-02-17 10:53:54 +0530

    addition slip

---
+++ src/app/@theme/model/additon-slip.ts
@@ -0,0 +1,27 @@
+export class AdditionSlip {
+  batchId: string;
+  dyeingSlipData: DyeingSlipData;
+  productionId: number;
+}
+
+export class DyeingSlipData {
+  controlId: number;
+  dyeingSlipItemData: DyeingSlipItemDatum[];
+  holdTime: number;
+  id: number;
+  isColor: boolean;
+  liquerRation: number;
+  processType: string;
+  sequence: number;
+  temp: number;
+}
+
+export class DyeingSlipItemDatum {
+  controlId: number;
+  id: number;
+  itemId: number;
+  itemName: string;
+  qty: number;
+  supplierId: number;
+  supplierName: string;
+}
--- src/app/pages/jet-planning/jet-planning.module.ts
+++ src/app/pages/jet-planning/jet-planning.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JetPlanningComponent } from './jet-planning.component';
 import { JetPlanningRoutingModule } from './jet-planning-routing.module';
-import { SharedModule } from 'app/@theme/shared.module';
 import { FormsModule } from '@angular/forms';
 import { ShadeWithBatchComponent } from '../production-planning/shade-with-batch/shade-with-batch.component';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { PlanningSlipComponent } from './planning-slip/planning-slip.component';
+ import { PlanningSlipComponent } from './planning-slip/planning-slip.component';
+import { SharedModule } from '../../@theme/shared.module';
 
 
 
 @NgModule({
-  declarations: [JetPlanningComponent,ShadeWithBatchComponent, PlanningSlipComponent],
+  declarations: [JetPlanningComponent,ShadeWithBatchComponent,PlanningSlipComponent ],
   imports: [
     CommonModule,
     JetPlanningRoutingModule,
-    SharedModule,
     FormsModule,
     ThemeModule,
-    DragDropModule
+    DragDropModule,
+    SharedModule
   ]
 })
 export class JetPlanningModule { }
