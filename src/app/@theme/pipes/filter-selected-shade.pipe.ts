114902e447f9984c87faa7f90dbb8c777ad336c9
SagarGloryAutotech <sagar.gloryautotech@gmail.com>
2021-03-09 18:33:15 +0530

    shade item filter fix

--- src/app/@theme/pipes/filter-selected-shade.pipe.ts
+++ src/app/@theme/pipes/filter-selected-shade.pipe.ts
@@ -2,13 +2,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({ name: "filterOutSelectedShade" })
 export class FilterSelectedShadePipe implements PipeTransform {
-  transform(supplierItemList: any, shadeItemList,refreshFlag, isUpdate) {
-    if(isUpdate && shadeItemList[shadeItemList.length-1].id && refreshFlag < shadeItemList.length){
-      return supplierItemList;
-    }
+  transform(supplierItemList: any, shadeItemList,refreshFlag, recordIndex) {
     if (shadeItemList && supplierItemList&& shadeItemList.length) {
       return supplierItemList.filter(
-        a => shadeItemList.findIndex(v => v.supplierItemId == a.id) == -1
+        a => shadeItemList.findIndex(v => v.supplierItemId == a.id) == -1 || shadeItemList[recordIndex].supplierItemId == a.id
       );
     } else {
         return supplierItemList;
