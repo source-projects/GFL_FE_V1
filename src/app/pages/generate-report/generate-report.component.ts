a292141c06dc13d279b4f406838ce307420193a8
Bhakti Madia <bhaktimadia@github.com>
2021-03-11 19:06:06 +0530

    report strctre

---
+++ src/app/pages/generate-report/generate-report.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ngx-generate-report",
+  templateUrl: "./generate-report.component.html",
+  styleUrls: ["./generate-report.component.scss"],
+})
+export class GenerateReportComponent implements OnInit {
+  colorTone = "#40f0f0";
+  constructor() {}
+
+  ngOnInit(): void {}
+}
