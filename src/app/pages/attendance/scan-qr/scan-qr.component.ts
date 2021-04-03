5c29eaf3726bd3127a9762de331886fc9c2f656c
kushal2868 <kushal.autotech@gmail.com>
2021-04-03 13:17:27 +0530

    scanner

--- src/app/pages/attendance/scan-qr/scan-qr.component.ts
+++ src/app/pages/attendance/scan-qr/scan-qr.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 import * as errorData from "../../../@theme/json/error.json";
 import { RegistrationService } from '../../../@theme/services/registration.service';
 
 @Component({
   selector: 'ngx-scan-qr',
   templateUrl: './scan-qr.component.html',
-  styleUrls: ['./scan-qr.component.scss']
+  styleUrls: ['./scan-qr.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class ScanQRComponent implements OnInit {
 
 
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+  hasDevices: boolean;
+  hasPermission: boolean;
+
+  qrResultString: string;
+
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
   
   loading = false;
   formSubmitted = false;
@@ -26,17 +39,68 @@ export class ScanQRComponent implements OnInit {
 
   ngOnInit(): void {}
 
-qrResultString: string;
+
+// clearResult(): void {
+//   this.qrResultString = null;
+  
+// }
+
+// onHasPermission(has: boolean) {
+//   this.hasPermission = has;
+// }
+
+// onCamerasFound(devices: MediaDeviceInfo[]): void {
+//   this.availableDevices = devices;
+//   console.log(this.availableDevices)
+//   this.hasDevices = Boolean(devices && devices.length);
+//   console.log(this.hasDevices)
+// }
+
+// onDeviceSelectChange(selected: string) {
+//   const device = this.availableDevices.find(x => x.deviceId === selected);
+//   this.currentDevice = device || null;
+// }
+
+// onCodeResult(resultString: string) {
+//   this.qrResultString = resultString;
+//   window.location.href = this.qrResultString;
+// }
 
 clearResult(): void {
   this.qrResultString = null;
-  
+}
+
+onCamerasFound(devices: MediaDeviceInfo[]): void {
+  this.availableDevices = devices;
+  this.hasDevices = Boolean(devices && devices.length);
+  alert(this.availableDevices[0].label)
 }
 
 onCodeResult(resultString: string) {
   this.qrResultString = resultString;
-  window.location.href = this.qrResultString;
 }
+
+onDeviceSelectChange(selected: string) {
+  const device = this.availableDevices.find(x => x.deviceId === selected);
+  this.currentDevice = device || null;
+}
+
+onHasPermission(has: boolean) {
+  this.hasPermission = has;
+}
+
+onTorchCompatible(isCompatible: boolean): void {
+  this.torchAvailable$.next(isCompatible || false);
+}
+
+toggleTorch(): void {
+  this.torchEnabled = !this.torchEnabled;
+}
+
+toggleTryHarder(): void {
+  this.tryHarder = !this.tryHarder;
+}
+
 
 onSave(){
   this.registrationService.empIdExistOrNot(this.empId).subscribe(
