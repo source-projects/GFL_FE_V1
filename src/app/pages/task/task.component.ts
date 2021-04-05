5a22f685c21f399dbe5469897d6450f3b6e3472a
Bhautik <bhautik.autotech@gmail.com>
2021-04-05 16:56:25 +0530

    task dashboard design

--- src/app/pages/task/task.component.ts
+++ src/app/pages/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule, OnInit } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { AddEditTaskComponent } from "./add-edit-task/add-edit-task.component";
+import { TaskDetailComponent } from "./task-detail/task-detail.component";
 
 @Component({
   selector: "ngx-task",
@@ -14,5 +15,8 @@ export class TaskComponent implements OnInit {
 
   openAddTaskComponent() {
     this.modalService.open(AddEditTaskComponent, { size: "lg" });
+  }
+  openDetail(){
+    this.modalService.open(TaskDetailComponent)
   }
 }
