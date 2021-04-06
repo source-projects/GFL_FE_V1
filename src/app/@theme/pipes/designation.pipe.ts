bf16fd0f381f291c4cf3c6d267036d83f9508791
SagarGloryAutotech <sagar.gloryautotech@gmail.com>
2021-04-06 16:52:07 +0530

    user designation fix

--- src/app/@theme/pipes/designation.pipe.ts
+++ src/app/@theme/pipes/designation.pipe.ts
@@ -6,10 +6,12 @@ export class DesignationFilterPipe implements PipeTransform {
   transform(
     list: any, isMaster:boolean
   ) {
-    if(!isMaster){
-      return list.filter(f=> f.designation.toLowerCase() == "team head")
-    }else{
-      return list.filter(f=> f.designation.toLowerCase() == "team member")
+    if(list && list.length){
+      if(!isMaster){
+        return list.filter(f=> f.designation.toLowerCase() == "team head")
+      }else{
+        return list.filter(f=> f.designation.toLowerCase() == "team member")
+      }
     }
   }
 }
