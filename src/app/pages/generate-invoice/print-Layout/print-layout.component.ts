4b9311945db3917f6bbdb13bd2d619007ef7d384
SagarGloryAutotech <72645682+SagarGloryAutotech@users.noreply.github.com>
2021-06-02 17:48:13 +0530

    Update print-layout.component.ts

--- src/app/pages/generate-invoice/print-Layout/print-layout.component.ts
+++ src/app/pages/generate-invoice/print-Layout/print-layout.component.ts
@@ -264,19 +264,18 @@ export class PrintLayoutComponent implements OnInit, OnDestroy {
       }
 
       if (!this.printInvoiceData[index].discount)
-        this.printInvoiceData[index].discount = (this.printInvoiceData[index].totalAmt * this.discount) / 100;
+                this.printInvoiceData[index].discount = Number(((this.printInvoiceData[index].totalAmt * this.discount) / 100).toFixed(2));
 
-      if (!this.printInvoiceData[index].sgst)
-        this.printInvoiceData[index].sgst = this.printInvoiceData[index].cgst = this.printInvoiceData[index].totalAmt * 0.025;
+              if (!this.printInvoiceData[index].taxAmt)
+                this.printInvoiceData[index].taxAmt = Number((this.printInvoiceData[index].totalAmt - this.printInvoiceData[index].discount).toFixed(2));
 
-      if (!this.printInvoiceData[index].taxAmt)
-        this.printInvoiceData[index].taxAmt = this.printInvoiceData[index].totalAmt - this.printInvoiceData[index].discount;
+              if (!this.printInvoiceData[index].sgst)
+                this.printInvoiceData[index].sgst = this.printInvoiceData[index].cgst = Number((this.printInvoiceData[index].taxAmt * 0.025).toFixed(2));
 
-      if (!this.printInvoiceData[index].netAmt) {
-        this.printInvoiceData[index].netAmt =
-          this.printInvoiceData[index].sgst + this.printInvoiceData[index].cgst + this.printInvoiceData[index].taxAmt;
-        this.printInvoiceData[index].netAmt = Math.round(this.printInvoiceData[index].netAmt);
-      }
+              if (!this.printInvoiceData[index].netAmt) {
+                this.printInvoiceData[index].netAmt =
+                  this.printInvoiceData[index].sgst + this.printInvoiceData[index].cgst + this.printInvoiceData[index].taxAmt;
+              }
 
     }
   }
