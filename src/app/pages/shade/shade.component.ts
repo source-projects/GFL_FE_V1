ea89d61c8d6586ba93af2f15fdb8fe743ce830fd
kushal2868 <kushal.autotech@gmail.com>
2021-07-15 13:46:28 +0530

    shade fix

--- src/app/pages/shade/shade.component.ts
+++ src/app/pages/shade/shade.component.ts
@@ -171,8 +171,9 @@ export class ShadeComponent implements OnInit, OnDestroy {
         if (data["success"]) {
           if (data["data"].length > 0) {
             this.shadeList = data["data"];
-            this.totalAmount = 0;
+
             this.shadeList.forEach(ele => {
+              this.totalAmount = 0;
               if (ele.shadeDataList && ele.shadeDataList.length) {
                 ele.shadeDataList.forEach((e) => {
                   if (e.amount) this.totalAmount += e.amount;
@@ -259,15 +260,19 @@ export class ShadeComponent implements OnInit, OnDestroy {
           count = i;
 
         });
-        if(count == 0){
+        if (count == 0) {
           this.avgCostPerWeight = sumWeight.toFixed(2);
-          this.avgCostPerMeter = sumMeter.toFixed(2); 
+          this.avgCostPerMeter = sumMeter.toFixed(2);
         }
-        else{
+        else {
           this.avgCostPerWeight = (sumWeight / count).toFixed(2);
-          this.avgCostPerMeter = (sumMeter / count).toFixed(2);  
+          this.avgCostPerMeter = (sumMeter / count).toFixed(2);
         }
         this.averageFlag = true;
+      }
+      else {
+        this.avgCostPerMeter = 0;
+        this.avgCostPerWeight = 0;
       }
     }
     else {
@@ -277,7 +282,7 @@ export class ShadeComponent implements OnInit, OnDestroy {
     }
 
     this.cdr.detectChanges();
-    
+
   }
 
   matchString(item, key, searchString) {
@@ -370,7 +375,13 @@ export class ShadeComponent implements OnInit, OnDestroy {
     }
   }
 
-  toggleChange(){
+  toggleChange(val) {
+    if(val){
+      this.searchANDCondition = true;
+    }
+    else{
+      this.searchANDCondition = false;
+    }
     this.filter();
   }
 }
