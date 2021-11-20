2ed4bb2680ecd466013f8be4876af1c7d16167f5
kushal2868 <kushal.autotech@gmail.com>
2021-11-20 10:40:38 +0530

    payment report chnges

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { NbMenuModule } from '@nebular/theme';
+
+import { SharedModule } from '../@theme/shared.module';
 import { ThemeModule } from '../@theme/theme.module';
-import { PagesComponent } from './pages.component';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
-import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
 import { PartyModule } from './party/party.module';
 import { QualityModule } from './quality/quality.module';
 import { SupplierModule } from './supplier/supplier.module';
-import {UserModule} from './user/user.module';
-import { InputDataComponent } from './input-data/input-data/input-data.component';
-import {SharedModule} from '../@theme/shared.module';
+import { UserModule } from './user/user.module';
+
 //import { InputDataModule } from './input-data/input-data.module';
 
 
